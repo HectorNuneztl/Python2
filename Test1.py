3c7cada0e532ff5b54ebcb7ef78ed1f3c5af56fd
Hector Nunez <jesushex2@gmail.com>
2025-03-22 10:50:43 -0700

    Test1.2

--- Test1.py
+++ Test1.py
@@ -5,6 +5,6 @@ uname = input("Introduce your user name: ")
 passw = input("\nIntroduce yor password: ")
 
 if uname == "HJ" and passw == "Olivion":
-    print("You have succefully log in")
+    print("Hellow "+uname+" you have succefully log in")
 else :
     print("Not available to log in, pleas try again")

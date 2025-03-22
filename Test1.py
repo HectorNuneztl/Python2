f3fb822711127ccbc8fd981b18847b8a166df19f
Hector Nunez <jesushex2@gmail.com>
2025-03-22 10:49:10 -0700

    Tests1

---
+++ Test1.py
@@ -0,0 +1,10 @@
+
+uname = input("Introduce your user name: ")
+
+
+passw = input("\nIntroduce yor password: ")
+
+if uname == "HJ" and passw == "Olivion":
+    print("You have succefully log in")
+else :
+    print("Not available to log in, pleas try again")
